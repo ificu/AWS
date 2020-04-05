c88213c2e7544f2497a1c4e92b29ffbb45aa1b60
최범진 <bj.choi.ificu@gmail.com>
2020-04-05 21:58:34 +0900

    .

---
+++ src/main/java/com/ificu/aws/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.ificu.aws.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
---
+++ src/main/java/com/ificu/aws/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.ificu.aws.springboot.web.dto;
+
+import com.ificu.aws.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
