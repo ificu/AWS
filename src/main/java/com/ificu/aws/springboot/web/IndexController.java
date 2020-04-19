29aa812746e3ad633cfd7939e6a25f129ed74277
최범진 <bj.choi.ificu@gmail.com>
2020-04-19 21:56:40 +0900

    .

--- src/main/java/com/ificu/aws/springboot/web/IndexController.java
+++ src/main/java/com/ificu/aws/springboot/web/IndexController.java
@@ -1,6 +1,8 @@
 package com.ificu.aws.springboot.web;
 
 import com.ificu.aws.springboot.Service.posts.PostsService;
+import com.ificu.aws.springboot.config.auth.LoginUser;
+import com.ificu.aws.springboot.config.auth.dto.SessionUser;
 import com.ificu.aws.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,15 +10,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
